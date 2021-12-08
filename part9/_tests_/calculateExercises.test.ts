13f49a0f3c138e515886f3d0b81007cbc1951cfb
Phil Smithies <philsmithies@gmail.com>
2021-12-08 17:06:39 +0100

    calculate exercises final

--- part9/_tests_/calculateExercises.test.ts
+++ part9/_tests_/calculateExercises.test.ts
@@ -1,5 +1,13 @@
 const calculateExercise = require("../calculateExercises.ts");
 
 it("can work out the amount of days that had exercise", () => {
-  expect(calculateExercise([3, 0, 2, 4.5, 0, 3, 1])).toBe(7);
+  expect(calculateExercise([3, 0, 2, 4.5, 0, 3, 1], 2)).toStrictEqual({
+    periodLength: 7,
+    trainingDays: 5,
+    success: false,
+    rating: 2,
+    ratingDescription: "not too bad but could be better",
+    target: 2,
+    average: 1.9285714285714286,
+  });
 });
--- part9/calculateExercises.ts
+++ part9/calculateExercises.ts
@@ -1,5 +1,58 @@
-const calculateExercises = (exercises: Array<number>, target: Number) => {
-  return exercises.length;
+interface exerciseValues {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (
+  exercises: Array<number>,
+  target: number
+): exerciseValues => {
+  const periodLength = exercises.length;
+  const trainingDays = exercises.filter((entry) => entry > 0).length;
+  const total = exercises.reduce((x, y) => x + y, 0);
+  const average = total / periodLength;
+  const success = average > target;
+  let rating = 0;
+  let ratingDescription = "";
+
+  switch (true) {
+    case average < 1:
+      rating = 1;
+      break;
+    case average < 2:
+      rating = 2;
+      break;
+    case average < 2:
+      rating = 3;
+      break;
+  }
+
+  switch (true) {
+    case average < 1:
+      ratingDescription = "room for improvement";
+      break;
+    case average < 2:
+      ratingDescription = "not too bad but could be better";
+      break;
+    case average < 2:
+      ratingDescription = "well done";
+      break;
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    average,
+    success,
+    target,
+    rating,
+    ratingDescription,
+  };
 };
 
 module.exports = calculateExercises;
