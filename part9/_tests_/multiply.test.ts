2fffbf4d43cf528ab4073767f2d0f972f684ae43
Phil Smithies <philsmithies@gmail.com>
2021-12-08 11:52:31 +0100

    tests

---
+++ part9/_tests_/multiply.test.ts
@@ -0,0 +1,7 @@
+const multiply = require("../multiply");
+
+test("can multiply 4 * 2 to equal 8", () => {
+  expect(multiply(4, 2, "The Multiplication result is ")).toBe(
+    "The Multiplication result is 8"
+  );
+});
