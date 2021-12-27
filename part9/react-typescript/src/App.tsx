a1810916a5fd429350e5472da964183444c253ac
Phil Smithies <philsmithies@gmail.com>
2021-12-27 10:10:03 +0100

    update

--- part9/react-typescript/src/App.tsx
+++ part9/react-typescript/src/App.tsx
@@ -4,21 +4,66 @@ import Content from "./Content";
 import Total from "./Total";
 
 const App = () => {
-  const courseName = "Half Stack application development";
-  const courseParts = [
+  // new types
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+
+  interface CourseDescription extends CoursePartBase {
+    description: string;
+  }
+  interface CourseNormalPart extends CoursePartBase {
+    type: "normal";
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+
+  interface CourseSubmissionPart extends CoursePartBase {
+    type: "submission";
+    description: string;
+    exerciseSubmissionLink: string;
+  }
+
+  type CoursePart =
+    | CourseNormalPart
+    | CourseDescription
+    | CourseProjectPart
+    | CourseSubmissionPart;
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal",
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the hard course part",
+      type: "normal",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission",
     },
   ];
+
+  const courseName = "Half Stack Application Development";
 
   return (
     <div>
---
+++ part9/react-typescript/src/Part.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+/// Then create a component Part that renders all attributes of each type of course part. Use a switch case-based exhaustive type checking! Use the new component in component Content.
+
+const Part = ({ courseParts }: { courseParts: Array }) => {
+  courseParts.forEach((part) => {});
+
+  return <div></div>;
+};
+
+export default Part;
