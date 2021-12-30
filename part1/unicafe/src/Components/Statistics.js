2e53c5be5d6bcda1c723f9722fbcdb541a4b4018
Phil Smithies <philsmithies@gmail.com>
2021-12-30 09:53:23 +0100

    conditional renders when no feedback given

--- part1/unicafe/src/Components/Statistics.js
+++ part1/unicafe/src/Components/Statistics.js
@@ -7,14 +7,22 @@ export const Statistics = ({
   findAverage,
 }) => {
   return (
-    <div>
+    <>
       <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Bad {bad}</p>
-      <p>Neutral {neutral}</p>
-      <p>All {findTotal}</p>
-      <p>Average {findAverage}</p>
-      <p>Amount Positive {findPositive}</p>
-    </div>
+      {findTotal > 0 ? (
+        <div>
+          <p>Good {good}</p>
+          <p>Bad {bad}</p>
+          <p>Neutral {neutral}</p>
+          <p>All {findTotal}</p>
+          <p>Average {findAverage}</p>
+          <p>Amount Positive {findPositive}</p>
+        </div>
+      ) : (
+        <div>
+          <p>No Feedback Given</p>
+        </div>
+      )}
+    </>
   );
 };
