74ec41e77ba1d2d0c7333287413142a7d0243a58
Phil Smithies <philsmithies@gmail.com>
2022-01-05 09:36:59 +0100

    handle change added

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,12 +4,21 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addToPhonebook = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <div>debug: {newName}</div>
+      <form onChange={addToPhonebook}>
         <div>
-          name: <input />
+          name: <input onChange={handleChange} />
         </div>
         <div>
           <button type="submit">add</button>
