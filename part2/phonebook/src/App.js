b1f7835baf097861c3a8d8866d4326e7bcf04358
Phil Smithies <philsmithies@gmail.com>
2022-01-05 11:35:14 +0100

    phone book step 2 - checking for existing values

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,19 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
   const addToPhonebook = (e) => {
     e.preventDefault();
-    const personObject = { name: newName };
-    setPersons(persons.concat(personObject));
-    setNewName("");
+    if (persons.filter((person) => person.name === newName).length > 0) {
+      setErrorMsg(`${newName} is already added to phonebook`);
+      setTimeout(() => {
+        setErrorMsg("");
+      }, 2000);
+    } else {
+      const personObject = { name: newName };
+      setPersons(persons.concat(personObject));
+    }
   };
 
   const handleChange = (e) => {
@@ -18,6 +25,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <h2 style={{ color: "red" }}>{errorMsg}</h2>
       <div>debug: {newName}</div>
       <form onSubmit={addToPhonebook}>
         <div>
