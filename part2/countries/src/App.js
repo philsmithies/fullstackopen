1a859abd84ef91e4f05ca84ef41cf1b7c7fb0dc4
Phil Smithies <philsmithies@gmail.com>
2022-01-22 12:32:24 +0100

    countries api for search terms done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [countries, setCountries] = useState([]);
+  const [result, setResult] = useState([]);
   const [query, setQuery] = useState("");
 
   useEffect(() => {
@@ -12,10 +12,44 @@ function App() {
     });
   }, []);
 
+  const searchTerm = () => {
+    axios
+      .get(`https://restcountries.com/v3.1/name/${query}`)
+      .then((response) => {
+        if (response.data.length > 10) {
+          console.log("Too Many Results");
+        } else {
+          setResult(response.data);
+        }
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setQuery(e.target.value);
+  };
+
   return (
     <div className="App">
       <h1>Search for a country</h1>
-      <input type="text" placeholder="Enter A Country" />
+      <input
+        type="text"
+        placeholder="Enter A Country"
+        onChange={(e) => handleChange(e)}
+      />
+      <button onClick={searchTerm()}>Search</button>
+      <h1>The query is {query}</h1>
+      {result &&
+        result.map((country) => {
+          return (
+            <div>
+              <p>{country.name.common}</p>
+            </div>
+          );
+        })}
     </div>
   );
 }
