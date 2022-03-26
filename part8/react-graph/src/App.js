f4bcddc343c5b510555711acadbdbab4bf7c7e69
Phil Smithies <philsmithies@gmail.com>
2022-03-26 08:06:58 +0100

    pre exercise

--- part8/react-graph/src/App.js
+++ part8/react-graph/src/App.js
@@ -1,28 +1,40 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import PersonForm from "./components/PersonForm";
 import { Persons } from "./components/Persons";
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`;
+import { ALL_PERSONS } from "./queries";
+import { useState } from "react";
+import PhoneForm from "./components/PhoneForm";
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
   );
+};
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
+  }
+  return <div style={{ color: "red" }}>{errorMessage}</div>;
 };
 
 export default App;
