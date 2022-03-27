2a2217e6452b6291674c8f94854b4e94649f0dad
Phil Smithies <philsmithies@gmail.com>
2022-03-27 09:46:54 +0200

    exercise time

--- part8/react-graph/src/App.js
+++ part8/react-graph/src/App.js
@@ -1,17 +1,16 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useApolloClient } from "@apollo/client";
 import PersonForm from "./components/PersonForm";
 import { Persons } from "./components/Persons";
 import { ALL_PERSONS } from "./queries";
 import { useState } from "react";
 import PhoneForm from "./components/PhoneForm";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
-
-  if (result.loading) {
-    return <div>loading...</div>;
-  }
+  const client = useApolloClient();
 
   const notify = (message) => {
     setErrorMessage(message);
@@ -20,9 +19,30 @@ const App = () => {
     }, 10000);
   };
 
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
